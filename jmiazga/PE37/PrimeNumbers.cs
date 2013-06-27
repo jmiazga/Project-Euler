0c96cda5dba7ca6d781236983205771c8e834c7a
jmiazga <jmiazga@gmail.com>
2013-06-27 11:52:57 -0400

    challenge 37 - truncatable prime numbers

---
+++ jmiazga/PE37/PrimeNumbers.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE37
+{
+    public class PrimeNumbers
+    {
+        private delegate int RemoveDigit(int n);
+
+        public bool[] Generate(int n)
+        {
+            bool[] a = new bool[1];
+
+            if (n > 1)
+            {
+                a = GetInitializedArray(n);
+
+                for (int i = 2; i <= Math.Sqrt(n); i++)
+                {
+                    if (a[i])
+                    {
+                        for (int j = i * i; j <= n; j += i)
+                        {
+                            a[j] = false;
+                        }
+                    }
+                }
+            }
+
+            return a;
+        }
+
+        private bool[] GetInitializedArray(int n)
+        {
+            bool[] a = new bool[n + 1];
+
+            for (int i = 2; i < a.Length; i++)
+                a[i] = true;
+
+            return a;
+        }
+
+        public List<int> GetTruncatable(bool[] a)
+        {
+            List<int> truncatablePrimes = new List<int>();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] && IsTruncatable(i, a, RemoveFirstDigit) && IsTruncatable(i, a, RemoveLastDigit))
+                    truncatablePrimes.Add(i);
+            }
+
+            return truncatablePrimes;
+        }
+
+        private bool IsTruncatable(int prime, bool[] primeArray, RemoveDigit removeDigit)
+        {
+            if (prime < 10)
+                return false;
+
+            int n = removeDigit(prime);
+
+            do
+            {
+                if (primeArray[n])
+                    n = removeDigit(n);
+                else
+                    return false;
+            }
+            while (n > 0);
+
+            return true;
+        }
+
+        private int RemoveFirstDigit(int n)
+        {
+            if (n < 10)
+                return -1;
+
+            return Convert.ToInt32(n.ToString().Substring(1));
+        }
+
+        private int RemoveLastDigit(int n)
+        {
+            return n / 10;
+        }
+    }
+}
---
+++ jmiazga/PE37/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrimeNumbers primeNumbers = new PrimeNumbers();
+            DateTime start = DateTime.Now;
+            bool[] primes = primeNumbers.Generate(750000);
+            List<int> truncatablePrimes = primeNumbers.GetTruncatable(primes);
+            int sum = truncatablePrimes.Sum();
+            DateTime end = DateTime.Now;
+            TimeSpan span = end - start;
+        }
+    }
+}
